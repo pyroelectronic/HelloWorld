3ec4ef78fedb57d8b004b6ba983aa01afe8dcc42
Johannes Samuel Friedrich <johannes.samuel.friedrich@ei.thm.de>
2019-10-02 12:18:22 +0200

    c2

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,16 @@ void function_hello_world()
 
 int main(void)
 {
+	
+	if(0)
+	{
+		int x = 10;
+	}
+	
 	function_hello_world();
 	
 	char local_array[10]={0};
-	local_array[9]=100;
+	local_array[9]=100+x;
 	
 	return 0;
 
